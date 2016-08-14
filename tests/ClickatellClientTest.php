c91dd9f6b9d88889dd88f424a299e9cf3a8f8d5e
Etienne Marais <hello@etiennemarais.co.za>
2016-08-14 22:07:26 +0200

    Placeholder for the Client test, TODO

---
+++ tests/ClickatellClientTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace NotificationChannels\Clickatell\Test;
+
+class ClickatellClientTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        // TODO Implement this
+    }
+}
