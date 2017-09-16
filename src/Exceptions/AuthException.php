fe1223312e5a0c452a2cd82b9b9ca275ae489556
anufriev <loobooger@gmail.com>
2017-09-16 14:39:44 +0300

    Minor changes

--- src/Exceptions/AuthException.php
+++ src/Exceptions/AuthException.php
@@ -4,43 +4,43 @@ namespace NotificationChannels\TurboSms\Exceptions;
 
 class AuthException extends \Exception
 {
-    /**
-     * @param string $message
-     *
-     * @return static
-     */
-    public static function NeedMoreParams(string $message)
-    {
-        return new static('Bad Request: "{$message}"', 400);
-    }
-
-    /**
-     * @param string $message
-     *
-     * @return static
-     */
-    public static function WrongCredentials(string $message)
-    {
-        return new static('Unauthorized: "{$message}"', 401);
-    }
-
-    /**
-     * @param string $message
-     *
-     * @return static
-     */
-    public static function AccountError(string $message)
-    {
-        return new static('Account error: "{$message}"', 403);
-    }
-
-    /**
-     * @param string $message
-     *
-     * @return static
-     */
-    public static function serviceRespondedWithAnError(string $message)
-    {
-        return new static('Auth error: "{$message}"', 409);
-    }
+	/**
+	 * @param string $message
+	 *
+	 * @return static
+	 */
+	public static function NeedMoreParams ( string $message )
+	{
+		return new static( 'Bad Request: "{$message}"', 400 );
+	}
+	
+	/**
+	 * @param string $message
+	 *
+	 * @return static
+	 */
+	public static function WrongCredentials ( string $message )
+	{
+		return new static( 'Unauthorized: "{$message}"', 401 );
+	}
+	
+	/**
+	 * @param string $message
+	 *
+	 * @return static
+	 */
+	public static function AccountError ( string $message )
+	{
+		return new static( 'Account error: "{$message}"', 403 );
+	}
+	
+	/**
+	 * @param string $message
+	 *
+	 * @return static
+	 */
+	public static function serviceRespondedWithAnError ( string $message )
+	{
+		return new static( 'Auth error: "{$message}"', 400 );
+	}
 }
--- src/Exceptions/CouldNotSendNotification.php
+++ src/Exceptions/CouldNotSendNotification.php
@@ -4,13 +4,37 @@ namespace NotificationChannels\TurboSms\Exceptions;
 
 class CouldNotSendNotification extends \Exception
 {
-    /**
-     * @param string $message
-     *
-     * @return static
-     */
-    public static function serviceRespondedWithAnError(string $message)
-    {
-        return new static('TurboSMS responded with an error "{$message}"');
-    }
+	/**
+	 * @param string $message
+	 *
+	 * @return static
+	 */
+	public static function serviceRespondedWithAnError ( string $message )
+	{
+		return new static( 'TurboSMS responded with an error "{$message}"', 424 );
+	}
+	
+	/**
+	 * @return static
+	 */
+	public static function SenderRequired ()
+	{
+		return new static( 'Notification was not sent. Sender must be defined.', 400 );
+	}
+	
+	/**
+	 * @return static
+	 */
+	public static function RecipientRequired ()
+	{
+		return new static( 'Notification was not sent. Recipient must be defined.', 400 );
+	}
+	
+	/**
+	 * @return static
+	 */
+	public static function MessageRequired ()
+	{
+		return new static( 'Notification was not sent. Message must be defined.', 400 );
+	}
 }
