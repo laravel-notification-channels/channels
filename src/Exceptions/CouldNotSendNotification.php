e7906c6abdee28de33f32551eadd53440f994302
Manash Sonowal <manash149@gmail.com>
2018-04-04 11:37:12 +0530

    Update CouldNotSendNotification.php

--- src/Exceptions/CouldNotSendNotification.php
+++ src/Exceptions/CouldNotSendNotification.php
@@ -4,8 +4,8 @@ namespace NotificationChannels\Textlocal\Exceptions;
 
 class CouldNotSendNotification extends \Exception
 {
-    public static function serviceRespondedWithAnError($exception)
+    public static function serviceRespondedWithAnError($exception, $message = null)
     {
-        return new static('Could Not Send SMS : '.$exception->getMessage());
+        return new static('Could Not Send SMS : '.$exception->getMessage() . ' message: ' . $message);
     }
 }
