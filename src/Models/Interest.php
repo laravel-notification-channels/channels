3a634d544ecf9deeeac7c3fcdce0add69eae0183
Patrick Kivits <patrick@patrickkivits.nl>
2018-08-28 15:02:15 +0200

    Added PHP DocBlocks

--- src/Models/Interest.php
+++ src/Models/Interest.php
@@ -4,17 +4,34 @@ namespace NotificationChannels\ExpoPushNotifications\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Interest
+ * @package NotificationChannels\ExpoPushNotifications\Models
+ */
 class Interest extends Model
 {
+    /**
+     * @var string
+     */
     protected $table;
 
+    /**
+     * @var array
+     */
     protected $fillable = [
         'key',
         'value',
     ];
 
+    /**
+     * @var bool
+     */
     public $timestamps = false;
 
+    /**
+     * Interest constructor.
+     * @param array $attributes
+     */
     public function __construct(array $attributes = [])
     {
         $this->table = config('exponent-push-notifications.interests.database.table_name');
