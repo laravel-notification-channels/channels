d3d1917f8daa8c5ee32aba04715f41b0e7f2283d
Andrey Telesh <andrey@horoshop.com.ua>
2018-09-13 16:48:24 +0300

    Code cleanup

--- tests/IntercomChannelTest.php
+++ tests/IntercomChannelTest.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * @link      http://horoshop.ua
- *
- * @copyright Copyright (c) 2015-2018 Horoshop TM
- * @author    Andrey Telesh <andrey@horoshop.ua>
- */
 
 namespace FtwSoft\NotificationChannels\Intercom\Tests;
 
