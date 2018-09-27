e5c0757ed33d766e1dc4bd34b9e198bbba6dc292
Alymosul <alymosul@gmail.com>
2018-09-27 11:50:19 +0200

    Wording...

--- config/exponent-push-notifications.php
+++ config/exponent-push-notifications.php
@@ -1,10 +1,13 @@
 <?php
 
+/**
+ * Here you may define the configuration for the expo-notifications-driver.
+ * The expo-notifications-driver can guide the sdk to use `database` or `file` repositories.
+ * The database repository uses the same configuration for the database in your Laravel app.
+ */
+
 return [
     'interests' => [
-        /*
-         * Supported: "file", "database"
-         */
         'driver' => env('EXPONENT_PUSH_NOTIFICATION_INTERESTS_STORAGE_DRIVER', 'file'),
 
         'database' => [
