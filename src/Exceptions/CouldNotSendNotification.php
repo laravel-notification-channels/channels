5e4d081accacd66694a7af70b6f95b8d1e6d00c5
Andrés Herrera García <andreshg112@gmail.com>
2019-03-18 10:53:08 -0500

    Exception message modified.

--- src/Exceptions/CouldNotSendNotification.php
+++ src/Exceptions/CouldNotSendNotification.php
@@ -2,10 +2,13 @@
 
 namespace NotificationChannels\PusherApiNotifications\Exceptions;
 
+use function GuzzleHttp\json_encode;
+
 class CouldNotSendNotification extends \Exception
 {
     public static function serviceRespondedWithAnError($response)
     {
-        return new static("Descriptive error message.");
+        $encodedResponse = json_encode($response);
+        return new static("Message could not be sent: $encodedResponse");
     }
 }
