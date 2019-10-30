610ba8292a7b4de9c968a2bf7b5196faa4b94b91
Manash Sonowal <manash149@gmail.com>
2019-10-30 11:54:40 +0530

    Update textlocal.php

--- config/textlocal.php
+++ config/textlocal.php
@@ -1,6 +1,11 @@
 <?php 
 
 return [
+	'username'  => env('TEXTLOCAL_USERNAME'),
+	'password'  => env('TEXTLOCAL_PASSWORD'),
+	'hash'      => env('TEXTLOCAL_HASH'),
+	'api_key'   => env('TEXTLOCAL_API_KEY'),
+	'sender'    => env('TEXTLOCAL_SENDER'),
     'request_urls' => [
         'IN' => 'https://api.textlocal.in/',
         'UK' => 'https://api.txtlocal.com/'
