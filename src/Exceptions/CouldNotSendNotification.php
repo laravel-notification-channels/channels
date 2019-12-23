2a475c8e46fa5f7ba9c84ef7bc0a2054e69e643e
Simon <design@simonwoodard.com.au>
2019-12-23 11:09:04 +1100

    Update formatting

--- src/Exceptions/CouldNotSendNotification.php
+++ src/Exceptions/CouldNotSendNotification.php
@@ -6,21 +6,21 @@ class CouldNotSendNotification extends \Exception
 {
     public static function methodDoesNotExist()
     {
-        return new static("The toVodafone method does not exist in your notification class.");
+        return new static(`The toVodafone method does not exist in your notification class.`);
     }
 
     public static function incorrectMessageClass()
     {
-        return new static("Your notification is incorrectly formatted or needs to use an instance of the VodafoneMessage class.");
+        return new static(`Your notification is incorrectly formatted or needs to use an instance of the VodafoneMessage class.`);
     }
 
     public static function serviceUnknownResponse()
     {
-        return new static("Unknown response coming from the Vodafone API.");
+        return new static(`Unknown response coming from the Vodafone API.`);
     }
 
     public static function serviceRespondedWithAnError($response)
     {
-        return new static("Code: $response->code - $response->reason.");
+        return new static(`Code: $response->code - $response->reason.`);
     }
 }
