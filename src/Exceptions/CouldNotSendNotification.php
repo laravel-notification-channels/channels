c6242574878f8a9231864d98009c7bf9677fc90c
Simon <design@simonwoodard.com.au>
2019-12-23 11:06:22 +1100

    Handle more exceptions

--- src/Exceptions/CouldNotSendNotification.php
+++ src/Exceptions/CouldNotSendNotification.php
@@ -4,6 +4,21 @@ namespace NotificationChannels\Vodafone\Exceptions;
 
 class CouldNotSendNotification extends \Exception
 {
+    public static function methodDoesNotExist()
+    {
+        return new static("The toVodafone method does not exist in your notification class.");
+    }
+
+    public static function incorrectMessageClass()
+    {
+        return new static("Your notification is incorrectly formatted or needs to use an instance of the VodafoneMessage class.");
+    }
+
+    public static function serviceUnknownResponse()
+    {
+        return new static("Unknown response coming from the Vodafone API.");
+    }
+
     public static function serviceRespondedWithAnError($response)
     {
         return new static("Code: $response->code - $response->reason.");
