cba4eb72916f67661edec3aa2d1c9ca7669fe9a4
Maximilian Schöll <max@schoell.co>
2020-04-25 00:15:10 +0200

    Add SMS77ChannelTest.php

---
+++ tests/SMS77ChannelTest.php
@@ -0,0 +1,196 @@
+<?php
+
+namespace NotificationChannels\SMS77\Test;
+
+use Mockery;
+use GuzzleHttp\Psr7\Response;
+use PHPUnit\Framework\TestCase;
+use NotificationChannels\SMS77\SMS77;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Notifications\Notification;
+use NotificationChannels\SMS77\SMS77Channel;
+use NotificationChannels\SMS77\SMS77Message;
+
+/**
+ * Class SMS77ChannelTest.
+ */
+class SMS77ChannelTest extends TestCase
+{
+
+    /**
+     * @var array
+     */
+    protected  $expected_response = [
+        'success' => '100',
+        'debug' =>  'true',
+        'sms_type' => 'direct',
+        'messages' =>  [
+            'id' => 123,
+            'sender' => 'SMS',
+            'text' => "This is my message.",
+        ]
+    ];
+
+    public function testSmsIsSent()
+    {
+        $notification = new TestSmsNotification;
+        $notifiable = new TestNotifiable;
+
+        $sms77 = Mockery::mock(SMS77::class);
+        $channel = new SMS77Channel($sms77);
+
+        $sms77->shouldReceive('sendMessage')->once()->with([
+            'json' => 1,
+            'text' => 'This is my message.',
+            'to' => '5555555555'
+        ])
+            ->andReturns(new Response(200, [], json_encode($this->expected_response)));
+
+
+        $actual_response = $channel->send($notifiable, $notification);
+
+        self::assertSame($this->expected_response, $actual_response);
+    }
+
+    public function testSmsIsSentWithCustomFrom()
+    {
+        $notification = new TestSmsNotificationWithCustomFrom;
+        $notifiable = new TestNotifiable;
+
+        $channel = new SMS77Channel(
+            $sms77 = Mockery::mock(SMS77::class)
+        );
+
+        $sms77->shouldReceive('sendMessage')->once()
+            ->with([
+                'json' => 1,
+                'unicode' => 1,
+                'from' => '5554443333',
+                'to' => '5555555555',
+                'text' => 'This is my message.',
+            ])
+            ->andReturns(new Response(200, [], json_encode($this->expected_response)));
+
+        $actual_response = $channel->send($notifiable, $notification);
+
+        self::assertSame($this->expected_response, $actual_response);
+    }
+
+    public function testSmsSentWithDebugging()
+    {
+        $notification = new TestSmsNotificationWithDebiggung;
+        $notifiable = new TestNotifiable;
+
+        $channel = new SMS77Channel(
+            $sms77 = Mockery::mock(SMS77::class)
+        );
+
+        $sms77->shouldReceive('sendMessage')->once()
+            ->with([
+                'json' => 1,
+                'debug' => 1,
+                'from' => '5554443333',
+                'to' => '5555555555',
+                'text' => 'This is my message.',
+            ])
+            ->andReturns(new Response(200, [], json_encode($this->expected_response)));
+
+        $actual_response = $channel->send($notifiable, $notification);
+
+        self::assertSame($this->expected_response, $actual_response);
+    }
+
+    public function testSmsSendWithAllMessageOptions()
+    {
+        $notification = new TestSmsNotificationWithAllMessageOptions;
+        $notifiable = new TestNotifiable;
+
+        $channel = new SMS77Channel(
+            $sms77 = Mockery::mock(SMS77::class)
+        );
+
+        $sms77->shouldReceive('sendMessage')->once()
+            ->with([
+                'from' => '987654321',
+                'to' => '123456789',
+                'text' => 'This is my message.',
+                'delay' => '000000',
+                'debug' => 1,
+                'no_reload' => 1,
+                'unicode' => 1,
+                'flash' => 1,
+                'details' => 1,
+                'json' => 1
+            ])
+            ->andReturns(new Response(200, [], json_encode($this->expected_response)));
+
+        $actual_response = $channel->send($notifiable, $notification);
+
+        self::assertSame($this->expected_response, $actual_response);
+    }
+}
+
+
+class TestNotifiable
+{
+    use Notifiable;
+
+    public $phone_number = '5555555555';
+
+    public function routeNotificationForNexmo($notification)
+    {
+        return $this->phone_number;
+    }
+}
+
+/**
+ * Default notification.
+ */
+class TestSmsNotification extends Notification
+{
+    public function toSms77($notifiable)
+    {
+        return new SMS77Message('This is my message.');
+    }
+}
+
+/**
+ * Notification with custom sender.
+ */
+class TestSmsNotificationWithCustomFrom extends Notification
+{
+    public function toSms77($notifiable)
+    {
+        return (new SMS77Message('This is my message.'))->from('5554443333')->unicode();
+    }
+}
+
+/**
+ * Notification with custom sender.
+ */
+class TestSmsNotificationWithDebiggung extends Notification
+{
+    public function toSms77($notifiable)
+    {
+        return (new SMS77Message('This is my message.'))->from('5554443333')->debug();
+    }
+}
+
+/**
+ * Notification with all available options.
+ */
+class TestSmsNotificationWithAllMessageOptions extends Notification
+{
+    public function toSms77($notifiable)
+    {
+        return (new SMS77Message('This is my message.'))
+            ->to('123456789')
+            ->from('987654321')
+            ->delay('00000000')
+            ->debug()
+            ->noReload()
+            ->unicode()
+            ->flash()
+            ->details();
+    }
+}
