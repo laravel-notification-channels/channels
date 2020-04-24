885612bb2d74a1ee534e843f13b3774cc7767340
Maximilian Schöll <max@schoell.co>
2020-04-24 21:26:17 +0200

    Add exceptions

--- src/Exceptions/CouldNotSendNotification.php
+++ src/Exceptions/CouldNotSendNotification.php
@@ -4,8 +4,23 @@ namespace NotificationChannels\SMS77\Exceptions;
 
 class CouldNotSendNotification extends \Exception
 {
-    public static function serviceRespondedWithAnError($response)
+    public static function serviceRespondedWithAnError($message)
     {
-        return new static("Descriptive error message.");
+        return new static("SMS77 Response: " . $message);
+    }
+
+    public static function apiKeyNotProvided(): self
+    {
+        return new static("API key is missing.");
+    }
+
+    public static function serviceNotAvailable($message): self
+    {
+        return new static($message);
+    }
+
+    public static function phoneNumberNotProvided(): self
+    {
+        return new static("No phone number was provided.");
     }
 }
