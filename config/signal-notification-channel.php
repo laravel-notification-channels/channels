7a802aedbbe2f58eea4f80fe2a91222c896b6479
tehCh0nG <iamthechong@gmail.com>
2020-08-19 14:54:20 -0600

    Add files via upload

--- config/signal-notification-channel.php
+++ config/signal-notification-channel.php
@@ -6,4 +6,10 @@ return [
   * ex: +12345556789, +442012345678
   **/
   'username' => env('SIGNAL_USERNAME'),
+  'java_home' => env('JAVA_HOME'),
+
+ /**
+ * Path to signal_cli binary file
+ **/
+  'signal_cli' => env('SIGNAL_CLI_LOCATION'),
 ]
