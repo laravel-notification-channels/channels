d509d1704aa0030cd17fd5066873fb7ee443655e
msonowal <manash149@gmail.com>
2022-08-18 01:14:40 +0530

    Update textlocal.php config with sample/example stub for custom usage

--- config/textlocal.php
+++ config/textlocal.php
@@ -1,14 +1,26 @@
-<?php 
+<?php
 
 return [
-	'username'  => env('TEXTLOCAL_USERNAME'),
-	'password'  => env('TEXTLOCAL_PASSWORD'),
-	'hash'      => env('TEXTLOCAL_HASH'),
-	'api_key'   => env('TEXTLOCAL_API_KEY'),
-	'sender'    => env('TEXTLOCAL_SENDER'),
+    'username' => env('TEXTLOCAL_USERNAME'),
+    'password' => env('TEXTLOCAL_PASSWORD'),
+    'hash' => env('TEXTLOCAL_HASH'),
+    'api_key' => env('TEXTLOCAL_API_KEY'),
+    'sender' => env('TEXTLOCAL_SENDER', 'DOCONL'),
     'request_urls' => [
         'IN' => 'https://api.textlocal.in/',
-        'UK' => 'https://api.txtlocal.com/'
+        'UK' => 'https://api.txtlocal.com/',
     ],
-    'country'   => env('TEXTLOCAL_COUNTRY', 'IN'),
+    'country' => env('TEXTLOCAL_COUNTRY', 'IN'),
+    /*
+    // a sample way to organize custom config when using mutliple on demand config
+    // uncomment this for that purpose or feel free to use your own implementations
+    'UK' => [
+        'username' => env('TEXTLOCAL_USERNAME_UK'),
+        'password' => env('TEXTLOCAL_PASSWORD_UK'),
+        'hash' => env('TEXTLOCAL_HASH_UK'),
+        'api_key' => env('TEXTLOCAL_API_KEY_UK'),
+        'sender' => env('TEXTLOCAL_SENDER_UK', 'DOCONL'),
+        'country' => env('TEXTLOCAL_COUNTRY_UK', 'UK'),
+    ],
+    */
 ];
