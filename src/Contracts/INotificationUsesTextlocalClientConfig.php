1ae7cf2e051090c6df419f5d94a9d37da1ad1ea2
msonowal <manash149@gmail.com>
2022-08-18 00:27:46 +0530

    Update INotificationUsesTextlocalClientConfig.php
    Update IUsesTextlocalClientConfig.php

--- src/Contracts/INotificationUsesTextlocalClientConfig.php
+++ src/Contracts/INotificationUsesTextlocalClientConfig.php
@@ -5,6 +5,14 @@ namespace NotificationChannels\Textlocal\Contracts;
 interface INotificationUsesTextlocalClientConfig
 {
     /**
+     * This method would be responsible if the user wants to use custom config
+     * for the current notification otherwise will work as is meaning using the
+     * default config
+     * @param $notification
+     * @return bool
+     */
+    public function shouldUseCustomTextlocalConfig($notification): bool;
+    /**
      * @param $notifiable
      * @return array [$username, $hash, $apiKey, $country]
      */
