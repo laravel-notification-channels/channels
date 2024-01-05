b2840a8259927190b0921514dbaec70e9ef1b165
cengiz <conkal@ciu.edu.tr>
2024-01-05 17:09:11 +0200

    guzzle http

--- src/Ntfy.php
+++ src/Ntfy.php
@@ -3,6 +3,7 @@
 namespace NotificationChannels\Ntfy;
 
 
+use GuzzleHttp\Psr7\Request;
 use NotificationChannels\Ntfy\Exceptions\CouldNotSendNotification;
 
 class Ntfy
@@ -14,22 +15,23 @@ class Ntfy
     private $client;
     private $port;
 
-    public function __construct($host, $port, $username, $password, \Psr\Http\Client\ClientInterface $client)
+    public function __construct($host, $port, $username, $password)
     {
         $this->host = $host;
         $this->username = $username;
         $this->password = $password;
-        $this->client = $client;
+        $this->client = new \GuzzleHttp\Client();
         $this->port = $port;
     }
 
     public function send(NtfyMessage $message)
     {
         try {
-            $request = new \GuzzleHttp\Psr7\Request('POST', $this->host . ':' . $this->port, [
+            $response = $this->client->post($this->host . ':' . $this->port, [
                 'auth' => [$this->username, $this->password],
                 'headers' => [
                     'Content-Type' => 'application/json',
+                    'Accept' => 'application/json',
                 ],
                 'json' => [
                     "topic" => $message->topic,
@@ -43,9 +45,8 @@ class Ntfy
                     "actions" => [],
                 ],
             ]);
-            $response = $this->client->sendRequest($request);
         } catch (\Exception $e) {
-            throw new CouldNotSendNotification($e->getMessage());
+            throw new CouldNotSendNotification($e);
         }
         return $response;
     }
